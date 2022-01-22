ec388c0845dd61d27705f62cfb25179935028b7e
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-01-22 13:17:00 +0000

    class enemy_type() created; def enemy_approaches created, randomly generates 1, 2 or 3 and pushes forward the enemy type.

--- run.py
+++ run.py
@@ -1,5 +1,22 @@
 import random
 
+class enemy_type():
+    def __init__(self, job, hp, mp, atk, blk, matk, mblk, spd):
+        self.job = job
+        self.hp = hp
+        self.mp = mp
+        self.atk = atk
+        self.blk = blk
+        self.matk = matk
+        self.mblk = mblk
+        self.spd = spd  
+
+    def full_stats(self):
+        return """
+        \n JOB - {}\n HEALTH - {}\n MAGIC - {}\n ATTACK - {}\n BLOCK - {}\n M. ATTACK - {}\n M. DEFENSE - {}\n SPEED - {}\n
+        """.format(self.job, self.hp, self.mp, self.atk, self.blk, self.matk, self.mblk, self.spd)
+
+
 class player_job():
     def __init__(self, name, job, hp, mp, atk, blk, matk, mblk, spd):
         self.name = name
@@ -20,6 +37,28 @@ class player_job():
     def hp_boost(self):
         self.hp += 100
 
+def enemy_approaches():
+    """
+    randomly generates 1, 2 or 3 and pushes forward an enemy type
+    """
+
+    global goblin
+    global witch
+    global striga
+
+    global ENEMY
+    ENEMY = random.randint(1,3)
+
+    if ENEMY == 1:
+        goblin = enemy_type("Goblin", 300, 100, 5, 5, 1, 1, 3)
+        print(goblin.full_stats())
+    elif ENEMY == 2:
+        witch = enemy_type("Witch", 100, 300, 1, 1, 5, 5, 3)
+        print(witch.full_stats())
+    elif ENEMY == 3:
+        striga = enemy_type("Striga", 200, 200, 3, 3, 3, 3, 3)
+        print(striga.full_stats())
+
 def enemy_encounter():
     """
     randomly generates 1 or 2 as an integer; will be used to determine if there is an enemy encounter
@@ -31,6 +70,7 @@ def enemy_encounter():
     if ENCOUNTER == 2:
         print("you encountered an ememy")
         print(ENCOUNTER)
+        enemy_approaches()
     else:
         print("no enemy encounter")
         print(ENCOUNTER)
@@ -40,6 +80,7 @@ def story_arc_1():
     """
     inital story setup; currently a test function
     """
+
     print(stats.name + ", you wake up next to a dying fire. It's cold, wet and dark.")
     stats.hp_boost()
     print(stats.hp)
