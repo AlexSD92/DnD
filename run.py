84106a88d4dd07aad6bc5bb4a48eec99e08702b6
AlexSD92 <alex.spara.dominguez@gmail.com>
2022-01-23 09:07:59 +0000

    moved the while loop to combat(); now checks hp > 0 for player and enemy, then checks for speed; if hp < 0 in either player_action or enemy_action, GAME OVER

--- run.py
+++ run.py
@@ -51,37 +51,40 @@ class player_job():
     def heal(self):
         if self.mp > 0:
             self.mp -= 50
-            self.hp += 50
-        
+            self.hp += 50     
 
 def enemy_action():
     """
     produces enemy attack as either a magic or physical attack from random integer
     """
+    if enemy_stats.hp > 0:
   
-    enemy_choice = random.randint(1,2)
+        enemy_choice = random.randint(1,2)
 
-    if enemy_choice == 1:
-        print("The enemy performed a physical attack!")
-        enemy_stats.attack()
-        player_action()
-    elif enemy_choice == 2:
-        print("The enemy performed a magical attack!")
-        enemy_stats.magic_attack()
-        player_action()
+        if enemy_choice == 1:
+            print("The enemy performed a physical attack!")
+            enemy_stats.attack()
+            player_action()
+        elif enemy_choice == 2:
+            print("The enemy performed a magical attack!")
+            enemy_stats.magic_attack()
+            player_action()
+
+    else: 
+        print("You defeated the enemy!")   
 
 def player_action():
 
-    print(
-        """
-        1 - Physical Attack
-        2 - Magical Attack
-        3 - Heal
-        4 - Boon
-        """
-    )
+    if stats.hp > 0:
 
-    while enemy_stats.hp > 0:
+        print(
+            """
+            1 - Physical Attack
+            2 - Magical Attack
+            3 - Heal
+            4 - Boon
+            """
+        )
 
         player_choice = input("What will you do?")
         
@@ -103,20 +106,22 @@ def player_action():
             enemy_action()
         elif player_choice == "5":
             print(stats.full_stats())
-        elif enemy_stats.hp <= 0:
-            print("You defeated the enemy!")
-            break        
+
+    else:
+        print("You were felled by the enemy!")
+        print("GAME OVER")
 
 def combat():
     """
     decides who takes the turn first based on the speed object
     """
-    if enemy_stats.spd < stats.spd:
-        print("player goes first")
-        player_action()
-    else:
-        print("enemy goes first")
-        enemy_action()
+    while enemy_stats.hp > 0 and stats.hp > 0:
+        if enemy_stats.spd < stats.spd:
+            print("player goes first")
+            player_action()
+        else:
+            print("enemy goes first")
+            enemy_action()
 
 def enemy_approaches():
     """
@@ -158,9 +163,11 @@ def enemy_encounter():
         print(ENCOUNTER)
 
 def story_arc_2v2():
+    enemy_encounter()
     print("story arc 2")
 
 def story_arc_2v1():
+    enemy_encounter()
     print("story arc 2")
 
 def story_arc_1():
@@ -189,10 +196,6 @@ def story_arc_1():
     else:
         print("Please make a valid choice.")
 
-
-
-
-
 def player_job_selection():
     """
     player choice of class and name input
@@ -204,19 +207,19 @@ def player_job_selection():
     if player_choice == "warrior":
         pname = input("What is your name, warrior? ")
         pjob = "Warrior"
-        stats = player_job(pname, pjob, 300, 100, 5, 5, 1, 1, 3)
+        stats = player_job(pname, pjob, 300, 100, 100, 5, 1, 1, 3)
         # print(stats.full_stats())
 
     elif player_choice == "assassin":
         pname = input("What is your name, assassin? ")
         pjob = "Assassin"
-        stats = player_job(pname, pjob, 200, 200, 3, 1, 3, 1, 5)
+        stats = player_job(pname, pjob, 200, 200, 100, 1, 3, 1, 5)
         # print(stats.full_stats())
 
     elif player_choice == "mage":
         pname = input("What is your name, mage? ")
         pjob = "Mage"
-        stats = player_job(pname, pjob, 100, 300, 1, 1, 5, 5, 1)
+        stats = player_job(pname, pjob, 100, 300, 100, 1, 5, 5, 1)
         # print(stats.full_stats())
 
     story_arc_1()
